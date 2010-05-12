447737869b8ac852d51f4ed8de24b864d90aff40
Pablo Kraan <pablo.kraan@gmail.com>
2010-05-12 13:02:39 +0000

    JmsMessageUtils related changes: _Extrating some methods from toMessage method. _Replacing mockobjects by mockito in test _Replacing real Session by mocks in a couple of tests _Adding more tests to toMessage method
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.2.x@17233 bf997673-6b11-0410-b953-e057580c5b09

--- transports/jms/src/test/java/org/mule/transport/jms/JmsMessageUtilsTestCase.java
+++ transports/jms/src/test/java/org/mule/transport/jms/JmsMessageUtilsTestCase.java
@@ -10,34 +10,53 @@
 
 package org.mule.transport.jms;
 
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import org.mule.tck.AbstractMuleTestCase;
 
-import com.mockobjects.constraint.IsInstanceOf;
-import com.mockobjects.dynamic.C;
-import com.mockobjects.dynamic.Mock;
-
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 import javax.jms.BytesMessage;
-import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
 import javax.jms.MapMessage;
+import javax.jms.Message;
 import javax.jms.MessageFormatException;
+import javax.jms.ObjectMessage;
 import javax.jms.Session;
 import javax.jms.StreamMessage;
 import javax.jms.TextMessage;
 
-import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.command.ActiveMQBytesMessage;
+import org.apache.activemq.command.ActiveMQMapMessage;
+import org.apache.activemq.command.ActiveMQObjectMessage;
+import org.apache.activemq.command.ActiveMQStreamMessage;
+import org.apache.activemq.command.ActiveMQTextMessage;
 import org.apache.commons.collections.IteratorUtils;
 import org.apache.commons.collections.iterators.IteratorEnumeration;
 
 public class JmsMessageUtilsTestCase extends AbstractMuleTestCase
 {
     public static final String ENCODING = "UTF-8";
+    private static final String OBJECT_ID = "id1234";
+    private static final Integer NUMBER_VALUE = Integer.valueOf(7);
+    private static final String STRING_VALUE = "Hello";
+    private static final String OBJECT_KEY = "object";
+    private static final String NUMBER_KEY = "number";
+    private static final String STRING_KEY = "string";
 
     public void testHeaders()
     {
@@ -56,131 +75,243 @@ public class JmsMessageUtilsTestCase extends AbstractMuleTestCase
 
     public void testTextMessageNullContent() throws Exception
     {
-        Mock mockMessage = new Mock(TextMessage.class);
-        mockMessage.expectAndReturn("getText", null);
-
-        TextMessage mockTextMessage = (TextMessage) mockMessage.proxy();
-
-        byte[] result = JmsMessageUtils.toByteArray(mockTextMessage, JmsConstants.JMS_SPECIFICATION_102B, ENCODING);
+        TextMessage mockMessage = mock(TextMessage.class);
+        when(mockMessage.getText()).thenReturn(null);
+        
+        byte[] result = JmsMessageUtils.toByteArray(mockMessage, JmsConstants.JMS_SPECIFICATION_102B, ENCODING);
         assertNotNull(result);
         assertEquals("Should return an empty byte array.", 0, result.length);
 
-        mockMessage.verify();
+        verify(mockMessage).getText();
     }
 
-    public void testByteMessageNullContent() throws Exception
+    public void testByteMessageNullContentInJmsVersion_1_0_1() throws Exception
     {
-        // test for JMS 1.0.2-compliant code path
-        Mock mockMessage = new Mock(BytesMessage.class);
-        mockMessage.expect("reset");
-        mockMessage.expectAndReturn("readBytes", new IsInstanceOf(byte[].class), -1);
-        BytesMessage mockBytesMessage = (BytesMessage) mockMessage.proxy();
+        BytesMessage mockMessage1 = mock(BytesMessage.class);
+        when(mockMessage1.readBytes((byte[]) anyObject())).thenReturn(-1);
 
-        byte[] result = JmsMessageUtils.toByteArray(mockBytesMessage, JmsConstants.JMS_SPECIFICATION_102B, ENCODING);
-        assertNotNull(result);
-        assertEquals("Should return an empty byte array.", 0, result.length);
-        mockMessage.verify();
+        byte[] result1 = JmsMessageUtils.toByteArray(mockMessage1, JmsConstants.JMS_SPECIFICATION_102B,
+            ENCODING);
+        assertNotNull(result1);
+        assertEquals("Should return an empty byte array.", 0, result1.length);
+        verify(mockMessage1).reset();        
+    }
+    
+    public void testByteMessageNullContentInJmsVersion_1_1() throws Exception
+    {
+        BytesMessage mockMessage2 = mock(BytesMessage.class);
+        when(mockMessage2.getBodyLength()).thenReturn(Long.valueOf(0));
 
-        // test for JMS 1.1-compliant code path
-        mockMessage = new Mock(BytesMessage.class);
-        mockMessage.expect("reset");
-        mockMessage.expectAndReturn("getBodyLength", new Long(0));
-        mockBytesMessage = (BytesMessage) mockMessage.proxy();
-
-        result = JmsMessageUtils.toByteArray(mockBytesMessage, JmsConstants.JMS_SPECIFICATION_11, ENCODING);
-        assertNotNull(result);
-        assertEquals("Should return an empty byte array.", 0, result.length);
-        mockMessage.verify();
+        byte[] result2 = JmsMessageUtils.toByteArray(mockMessage2, JmsConstants.JMS_SPECIFICATION_11,
+            ENCODING);
+        assertNotNull(result2);
+        assertEquals("Should return an empty byte array.", 0, result2.length);
+        verify(mockMessage2).reset();  
     }
 
     public void testStreamMessageSerialization() throws Exception
     {
+        Session session = mock(Session.class);
+        when(session.createStreamMessage()).thenReturn(new ActiveMQStreamMessage());
+
+        // Creates a test list with data
+        List data = new ArrayList();
+        data.add(Boolean.TRUE);
+        data.add(new Byte("1"));
+        data.add(new Short("2"));
+        data.add(new Character('3'));
+        data.add(new Integer("4"));
+        // can't write Longs: https://issues.apache.org/activemq/browse/AMQ-1965
+        // data.add(new Long("5"));
+        data.add(new Float("6"));
+        data.add(new Double("7"));
+        data.add(new String("8"));
+        data.add(new byte[]{9, 10});
+
+        StreamMessage result = (StreamMessage) JmsMessageUtils.toMessage(data, session);
+
+        // Resets so it's readable
+        result.reset();
+        assertEquals(Boolean.TRUE, result.readObject());
+        assertEquals(new Byte("1"), result.readObject());
+        assertEquals(new Short("2"), result.readObject());
+        assertEquals(new Character('3'), result.readObject());
+        assertEquals(new Integer("4"), result.readObject());
+        // can't write Longs: https://issues.apache.org/activemq/browse/AMQ-1965
+        // assertEquals(new Long("5"), result.readObject());
+        assertEquals(new Float("6"), result.readObject());
+        assertEquals(new Double("7"), result.readObject());
+        assertEquals(new String("8"), result.readObject());
+        assertTrue(Arrays.equals(new byte[]{9, 10}, (byte[]) result.readObject()));
+    }
+
+    public void testStreamMessageSerializationWithInvalidType() throws Exception
+    {
         Session session = null;
+        session = mock(Session.class);
+        when(session.createStreamMessage()).thenReturn(new ActiveMQStreamMessage());
+
+        // Creates a test list with data
+        List data = new ArrayList();
+        data.add(new Object());
+
         try
         {
-            // get a live session
-            ConnectionFactory cf = new ActiveMQConnectionFactory("vm://localhost?broker.persistent=false&broker.useJmx=false");
-            session = cf.createConnection().createSession(false, Session.AUTO_ACKNOWLEDGE);
-
-            // create a test list with data
-            List data = new ArrayList();
-            data.add(new Object());
-
-            // test the invalid input
-            try
-            {
-                JmsMessageUtils.toMessage(data, session);
-                fail("Should've failed with MessageFormatException");
-            }
-            catch (MessageFormatException e)
-            {
-                // expected
-            }
-
-
-            // test valid types
-            data.clear();
-            data.add(Boolean.TRUE);
-            data.add(new Byte("1"));
-            data.add(new Short("2"));
-            data.add(new Character('3'));
-            data.add(new Integer("4"));
-            // can't write Longs: https://issues.apache.org/activemq/browse/AMQ-1965
-            //data.add(new Long("5"));
-            data.add(new Float("6"));
-            data.add(new Double("7"));
-            data.add(new String("8"));
-            data.add(new byte[] {9, 10});
-
-
-            StreamMessage result = (StreamMessage) JmsMessageUtils.toMessage(data, session);
-            // reset so it's readable
-            result.reset();
-
-            assertEquals(Boolean.TRUE, result.readObject());
-            assertEquals(new Byte("1"), result.readObject());
-            assertEquals(new Short("2"), result.readObject());
-            assertEquals(new Character('3'), result.readObject());
-            assertEquals(new Integer("4"), result.readObject());
-            // can't write Longs: https://issues.apache.org/activemq/browse/AMQ-1965
-            //assertEquals(new Long("5"), result.readObject());
-            assertEquals(new Float("6"), result.readObject());
-            assertEquals(new Double("7"), result.readObject());
-            assertEquals(new String("8"), result.readObject());
-
-            assertTrue(Arrays.equals(new byte[] {9, 10}, (byte[]) result.readObject()));
-
-
+            JmsMessageUtils.toMessage(data, session);
+            fail("Should've failed with MessageFormatException");
         }
-        finally
+        catch (MessageFormatException e)
         {
-            if (session != null)
-            {
-                session.close();
-            }
+            // expected
         }
     }
     
     public void testMapMessageWithNullValue() throws Exception
     {
-        String[] keys = new String[] { "key", "null" };
+        String[] keys = new String[]{"key", "null"};
         Iterator<String> keyIterator = IteratorUtils.arrayIterator(keys);
         Enumeration<String> keyEnumeration = new IteratorEnumeration(keyIterator);
-        
-        Mock mockMessage = new Mock(MapMessage.class);
-        mockMessage.expectAndReturn("getMapNames", keyEnumeration);
-        mockMessage.expectAndReturn("getObject", C.eq("key"), "value");
-        mockMessage.expectAndReturn("getObject", C.eq("null"), null);
-        MapMessage message = (MapMessage) mockMessage.proxy();
-        
-        Object result = JmsMessageUtils.toObject(message, JmsConstants.JMS_SPECIFICATION_11, ENCODING);
+
+        MapMessage mockMessage1 = mock(MapMessage.class);
+        when(mockMessage1.getMapNames()).thenReturn(keyEnumeration);
+        when(mockMessage1.getObject("key")).thenReturn("value");
+        when(mockMessage1.getObject("null")).thenReturn(null);
+
+        Object result = JmsMessageUtils.toObject(mockMessage1, JmsConstants.JMS_SPECIFICATION_11, ENCODING);
         assertNotNull(result);
         assertTrue(result instanceof Map);
         Map map = (Map) result;
         assertEquals("value", map.get("key"));
         assertNull(map.get("null"));
+    }
 
-        mockMessage.verify();
+    /**
+     * Tests that is able to convert a Map which only contains simple values into a
+     * MapMessage.
+     */
+    public void testConvertsValidMapToMapMessage() throws JMSException
+    {
+        Map<String, Object> message = new HashMap<String, Object>();
+        message.put(STRING_KEY, STRING_VALUE);
+        message.put(NUMBER_KEY, NUMBER_VALUE);
+
+        Session session = mock(Session.class);
+        when(session.createMapMessage()).thenReturn(new ActiveMQMapMessage());
+
+        MapMessage result = (MapMessage) JmsMessageUtils.toMessage(message, session);
+
+        assertEquals(STRING_VALUE, result.getString(STRING_KEY));
+        assertEquals(7, result.getInt(NUMBER_KEY));
+    }
+
+    /**
+     * Tests that trying to convert a Map which contains a non valid value throws an
+     * exception.
+     */
+    public void testConvertingMapIncludingNotValidValueThrowsException() throws JMSException
+    {
+        Map<String, Object> message = new HashMap<String, Object>();
+        message.put(STRING_KEY, STRING_VALUE);
+        message.put(OBJECT_KEY, new Object());
+
+        Session session = mock(Session.class);
+        when(session.createMapMessage()).thenReturn(new ActiveMQMapMessage());
+
+        try
+        {
+            JmsMessageUtils.toMessage(message, session);
+            fail("Able to convert an invalid Map instance: contains a non valid value");
+        }
+        catch (MessageFormatException expected)
+        {
+        }
+    }
+
+    public void testConvertingStringToTextMessage() throws JMSException
+    {
+        String text = "Hello world";
+
+        Session session = mock(Session.class);
+        TextMessage textMessage = new ActiveMQTextMessage();
+        textMessage.setText(text);
+        when(session.createTextMessage(text)).thenReturn(textMessage);
+
+        TextMessage message = (TextMessage) JmsMessageUtils.toMessage(text, session);
+        assertEquals(textMessage, message);
+        verify(session, times(1)).createTextMessage(text);
     }
     
+    public void testConvertingByteArrayToBytesMessage() throws JMSException {
+        Session session = mock(Session.class);
+        when(session.createBytesMessage()).thenReturn(new ActiveMQBytesMessage());
+        
+        byte[] bytesArray = new byte[] {1, 2};
+        BytesMessage message = (BytesMessage) JmsMessageUtils.toMessage(bytesArray, session);
+        
+        // Makes the message readable
+        message.reset();
+        byte[] bytesArrayResult = new byte[(int) message.getBodyLength()];
+        int length = message.readBytes(bytesArrayResult);        
+        assertEquals(2, length);
+        assertEquals(bytesArray[0],bytesArrayResult[0]);
+        assertEquals(bytesArray[1],bytesArrayResult[1]);
+    }
+
+    public void testConvertingSerializableToObjectMessage() throws JMSException
+    {
+        Session session = mock(Session.class);
+        when(session.createObjectMessage()).thenReturn(new ActiveMQObjectMessage());
+
+        ObjectMessage message = (ObjectMessage) JmsMessageUtils.toMessage(new SerializableObject(OBJECT_ID),
+            session);
+
+        Serializable serializable = message.getObject();
+        assertTrue(serializable instanceof SerializableObject);
+        assertEquals(OBJECT_ID, ((SerializableObject) serializable).id);
+    }
+
+    public void testConvertingMessageToMessageReturnsSameObject() throws JMSException
+    {
+        Message original = mock(Message.class);
+        Message result = JmsMessageUtils.toMessage(original, null);
+        assertSame(original, result);
+    }
+
+    public void testConvertingInvalidTypeThrowsException() throws JMSException
+    {
+        try
+        {
+            JmsMessageUtils.toMessage(new Object(), null);
+            fail("Able to convert to message an invalid type");
+        }
+        catch (JMSException e)
+        {
+            // Expected
+        }
+    }
+
+    /**
+     * Dummy serializable class used to test that conversion from Map to JmsMessage
+     * is OK when it includes non primitive serializable values.
+     */
+    private static class SerializableObject implements Serializable
+    {
+        private static final long serialVersionUID = -4865136673252075014L;
+        private String id;
+
+        public SerializableObject(String id)
+        {
+            this.id = id;
+        }
+
+        private void readObject(ObjectInputStream aInputStream) throws ClassNotFoundException, IOException
+        {
+            aInputStream.defaultReadObject();
+        }
+
+        private void writeObject(ObjectOutputStream aOutputStream) throws IOException
+        {
+            aOutputStream.defaultWriteObject();
+        }
+    }
 }
