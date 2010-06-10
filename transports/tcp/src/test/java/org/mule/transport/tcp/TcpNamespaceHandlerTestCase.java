72a47df97732123b5ec6e07fbf8e3483f1b52f97
esteban.robles <esteban.robles>
2010-06-10 09:15:32 +0000

    EE-1873 & EE-1845 - Add exception policies for handling exceptions to rewrite to clients - Add configurable message dispatcher that catches socket to improve performance - Add new protocol that supports a custom class loader - Improve XML schema for new features
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.2.x@17547 bf997673-6b11-0410-b953-e057580c5b09

--- transports/tcp/src/test/java/org/mule/transport/tcp/TcpNamespaceHandlerTestCase.java
+++ transports/tcp/src/test/java/org/mule/transport/tcp/TcpNamespaceHandlerTestCase.java
@@ -11,6 +11,7 @@ package org.mule.transport.tcp;
 
 import org.mule.tck.FunctionalTestCase;
 import org.mule.transport.tcp.TcpConnector;
+import org.mule.transport.tcp.protocols.CustomClassLoadingLengthProtocol;
 
 /**
  * TODO
@@ -37,7 +38,7 @@ public class TcpNamespaceHandlerTestCase extends FunctionalTestCase
         assertTrue(c.isKeepAlive());
         assertTrue(c.isConnected());
         assertTrue(c.isStarted());
-
+        assertEquals(c.getNextMessageExceptionPolicy().getClass(), DefaultMessageExceptionPolicy.class);
     }
     
     public void testSeparateTimeouts() throws Exception
@@ -48,6 +49,7 @@ public class TcpNamespaceHandlerTestCase extends FunctionalTestCase
         assertEquals(3000, c.getClientSoTimeout());
         assertTrue(c.isConnected());
         assertTrue(c.isStarted());
+        assertEquals(c.getNextMessageExceptionPolicy().getClass(), DefaultMessageExceptionPolicy.class);
     }
     
     public void testPollingConnector()
@@ -58,5 +60,35 @@ public class TcpNamespaceHandlerTestCase extends FunctionalTestCase
         assertEquals(3000, c.getClientSoTimeout());
         assertTrue(c.isConnected());
         assertTrue(c.isStarted());
+        assertEquals(c.getNextMessageExceptionPolicy().getClass(), DefaultMessageExceptionPolicy.class);
+    }
+    
+    public void testDefaultExceptionPolicy() throws Exception
+    {
+        TcpConnector c = (TcpConnector)muleContext.getRegistry().lookupConnector("defaultExceptionPolicyConnector");
+        assertNotNull(c);
+        assertEquals(c.getNextMessageExceptionPolicy().getClass(), DefaultMessageExceptionPolicy.class);
+    }
+
+    public void testRewriteExceptionPolicy() throws Exception
+    {
+        TcpConnector c = (TcpConnector)muleContext.getRegistry().lookupConnector("rewriteExceptionPolicyConnector");
+        assertNotNull(c);
+        assertEquals(c.getNextMessageExceptionPolicy().getClass(), RewriteMessageExceptionPolicy.class);
+    }
+    
+    public void testCustomClassLoadingProtocol() throws Exception
+    {
+        TcpConnector c = (TcpConnector)muleContext.getRegistry().lookupConnector("custom-class-loading-protocol-connector");
+        assertNotNull(c);
+        CustomClassLoadingLengthProtocol protocol = (CustomClassLoadingLengthProtocol) c.getTcpProtocol();
+        assertEquals(protocol.getClass(), CustomClassLoadingLengthProtocol.class);
+        assertEquals(protocol.getClassLoader(), muleContext.getRegistry().get("classLoader"));
+    }
+    
+    public void testMessageDispatcherFactoryConnector() throws Exception {
+        TcpConnector c = (TcpConnector)muleContext.getRegistry().lookupConnector("messageDispatcherFactoryConnector");
+        assertNotNull(c);
+        assertEquals(LocalSocketTcpMessageDispatcherFactory.class, c.getDispatcherFactory().getClass());
     }
 }
