cce9a5647688c307c2d0bf0541dd3e7347501622
esteban.robles <esteban.robles>
2010-06-28 13:34:17 +0000

    EE-1641: Please add support for JDBC Query timeouts - Copy data source from original query runner
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.2.x@17733 bf997673-6b11-0410-b953-e057580c5b09

--- transports/jdbc/src/main/java/org/mule/transport/jdbc/JdbcConnector.java
+++ transports/jdbc/src/main/java/org/mule/transport/jdbc/JdbcConnector.java
@@ -461,7 +461,9 @@ public class JdbcConnector extends AbstractConnector
         
         if (queryTimeout >= 0)
         {
-            return new ExtendedQueryRunner(queryTimeout);
+            ExtendedQueryRunner extendedQueryRunner = new ExtendedQueryRunner(queryTimeout);
+            extendedQueryRunner.setDataSource(this.queryRunner.getDataSource());
+            return extendedQueryRunner;
         }
         else
         {
