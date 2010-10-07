244e386f0356ed13e41d2a06830ae883dd853110
Dirk Olmes <dirk.olmes@mulesoft.com>
2010-10-07 12:24:07 +0000

    give the test some love
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.2.x@19859 bf997673-6b11-0410-b953-e057580c5b09

--- core/src/test/java/org/mule/mule/model/MethodHeaderEntryPointResolverTestCase.java
+++ core/src/test/java/org/mule/mule/model/MethodHeaderEntryPointResolverTestCase.java
@@ -20,78 +20,98 @@ import org.mule.transport.NullPayload;
 
 public class MethodHeaderEntryPointResolverTestCase extends AbstractMuleTestCase
 {
+    private MethodHeaderPropertyEntryPointResolver resolver;
+
+    @Override
+    protected void doSetUp() throws Exception
+    {
+        super.doSetUp();
+        resolver = new MethodHeaderPropertyEntryPointResolver();
+    }
+
     public void testMethodSetPass() throws Exception
     {
-        MethodHeaderPropertyEntryPointResolver resolver = new MethodHeaderPropertyEntryPointResolver();
         MuleEventContext ctx = getTestEventContext("blah");
         ctx.getMessage().setProperty("method", "someBusinessMethod");
+        
         InvocationResult result = resolver.invoke(new MultiplePayloadsTestObject(), ctx);
-        assertEquals(result.getState(), InvocationResult.STATE_INVOKED_SUCESSFUL);
+        assertInvocationWasSuccessful(result);
     }
 
     public void testMethodSetWithNoArgsPass() throws Exception
     {
-        MethodHeaderPropertyEntryPointResolver resolver = new MethodHeaderPropertyEntryPointResolver();
         MuleEventContext ctx = getTestEventContext(NullPayload.getInstance());
         ctx.getMessage().setProperty("method", "wash");
+        
         InvocationResult result = resolver.invoke(new Apple(), ctx);
-        assertEquals(result.getState(), InvocationResult.STATE_INVOKED_SUCESSFUL);
+        assertInvocationWasSuccessful(result);
         assertEquals("wash", result.getMethodCalled());
     }
 
     public void testCustomMethodProperty() throws Exception
     {
-        MethodHeaderPropertyEntryPointResolver resolver = new MethodHeaderPropertyEntryPointResolver();
         resolver.setMethodProperty("serviceMethod");
+        
         MuleEventContext ctx = getTestEventContext("blah");
         ctx.getMessage().setProperty("serviceMethod", "someBusinessMethod");
+        
         InvocationResult result = resolver.invoke(new MultiplePayloadsTestObject(), ctx);
-        assertEquals(result.getState(), InvocationResult.STATE_INVOKED_SUCESSFUL);
+        assertInvocationWasSuccessful(result);
     }
 
     public void testCustomMethodPropertyFail() throws Exception
     {
-        MethodHeaderPropertyEntryPointResolver resolver = new MethodHeaderPropertyEntryPointResolver();
         resolver.setMethodProperty("serviceMethod");
+        
         MuleEventContext ctx = getTestEventContext("blah");
         ctx.getMessage().setProperty("serviceMethod", "noMethod");
+        
         InvocationResult result = resolver.invoke(new MultiplePayloadsTestObject(), ctx);
-        assertEquals(result.getState(), InvocationResult.STATE_INVOKED_FAILED);
+        assertInvocationFailed(result);
     }
-
+    
     public void testMethodPropertyFail() throws Exception
     {
-        MethodHeaderPropertyEntryPointResolver resolver = new MethodHeaderPropertyEntryPointResolver();
         resolver.setMethodProperty("serviceMethod");
+        
         MuleEventContext ctx = getTestEventContext("blah");
         ctx.getMessage().setProperty("myMethod", "someBusinessMethod");
+        
         InvocationResult result = resolver.invoke(new MultiplePayloadsTestObject(), ctx);
-        assertEquals(result.getState(), InvocationResult.STATE_INVOKED_FAILED);
+        assertInvocationFailed(result);
     }
 
     public void testMethodPropertyMismatch() throws Exception
     {
-        MethodHeaderPropertyEntryPointResolver resolver = new MethodHeaderPropertyEntryPointResolver();
         MuleEventContext ctx = getTestEventContext("blah");
         ctx.getMessage().setProperty("method", "noMethod");
+        
         InvocationResult result = resolver.invoke(new MultiplePayloadsTestObject(), ctx);
-        assertEquals(result.getState(), InvocationResult.STATE_INVOKED_FAILED);
+        assertInvocationFailed(result);
     }
 
     /**
-     * If a method with correct name is available then it should be used is the
+     * If a method with correct name is available then it should be used if the
      * parameter type is assignable from the payload type and not just if there is an
      * exact match. See MULE-3636.
-     * 
-     * @throws Exception
      */
     public void testMethodPropertyParameterAssignableFromPayload() throws Exception
     {
-        MethodHeaderPropertyEntryPointResolver resolver = new MethodHeaderPropertyEntryPointResolver();
         MuleEventContext ctx = getTestEventContext(new Apple());
         ctx.getMessage().setProperty("method", "wash");
+
         InvocationResult result = resolver.invoke(new TestFruitCleaner(), ctx);
-        assertEquals(result.getState(), InvocationResult.STATE_INVOKED_SUCESSFUL);
+        assertInvocationWasSuccessful(result);
+    }
+    
+    private void assertInvocationWasSuccessful(InvocationResult result)
+    {
+        assertEquals(InvocationResult.STATE_INVOKED_SUCESSFUL, result.getState());        
+    }
+
+    private void assertInvocationFailed(InvocationResult result)
+    {
+        assertEquals(InvocationResult.STATE_INVOKED_FAILED, result.getState());        
     }
 
     public static class TestFruitCleaner
@@ -106,5 +126,4 @@ public class MethodHeaderEntryPointResolverTestCase extends AbstractMuleTestCase
             // dummy
         }
     }
-
 }
