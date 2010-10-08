31400314ff266d4b08afe95b7ef98cd687b54d2b
Dirk Olmes <dirk.olmes@mulesoft.com>
2010-10-08 06:56:32 +0000

    convert test to use dynamic ports
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.0.x@19864 bf997673-6b11-0410-b953-e057580c5b09

--- tests/integration/src/test/java/org/mule/test/integration/streaming/FileToTcpStreamingTestCase.java
+++ tests/integration/src/test/java/org/mule/test/integration/streaming/FileToTcpStreamingTestCase.java
@@ -10,12 +10,12 @@
 package org.mule.test.integration.streaming;
 
 
-import org.mule.tck.FunctionalTestCase;
+import org.mule.tck.DynamicPortTestCase;
 import org.mule.util.FileUtils;
 
 import java.io.File;
 
-public class FileToTcpStreamingTestCase extends FunctionalTestCase
+public class FileToTcpStreamingTestCase extends DynamicPortTestCase
 {
     @Override
     protected void doTearDown() throws Exception
@@ -27,6 +27,12 @@ public class FileToTcpStreamingTestCase extends FunctionalTestCase
     protected String getConfigResources()
     {
         return "org/mule/test/integration/streaming/file-to-tcp-streaming.xml";
+    }
+
+    @Override
+    protected int getNumPortsToFind()
+    {
+        return 1;
     }
 
     public void testStreamingFromFileToTcp() throws Exception
