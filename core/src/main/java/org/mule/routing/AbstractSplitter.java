cf52cd21f43a298d11e5876d4b195f38e1ee2175
Daniel Feist <dfeist@gmail.com>
2010-10-15 15:47:52 +0000

    Fix javadoc
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.0.x@19941 bf997673-6b11-0410-b953-e057580c5b09

--- core/src/main/java/org/mule/routing/AbstractSplitter.java
+++ core/src/main/java/org/mule/routing/AbstractSplitter.java
@@ -29,7 +29,7 @@ import java.util.List;
  * Implementations must implement {@link #splitMessage(MuleEvent)} and determine how
  * the message is split.
  * <p>
- * <b>EIP Reference:</b> <a href="http://www.eaipatterns.com/Sequencer.html"/>
+ * <b>EIP Reference:</b> <a href="http://www.eaipatterns.com/Sequencer.html">http://www.eaipatterns.com/Sequencer.html</a>
  */
 
 public abstract class AbstractSplitter extends
