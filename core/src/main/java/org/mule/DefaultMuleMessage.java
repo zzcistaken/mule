f2796f8d10ef3606a4d932c12866e80551e886c9
mike.schilling <mike.schilling>
2010-11-09 00:38:43 +0000

    MULE-5095 fix bug
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-3.0.x@20125 bf997673-6b11-0410-b953-e057580c5b09

--- core/src/main/java/org/mule/DefaultMuleMessage.java
+++ core/src/main/java/org/mule/DefaultMuleMessage.java
@@ -5,7 +5,7 @@
  *
  * The software in this package is published under the terms of the CPAL v1.0
  * license, a copy of which has been included with this distribution in the
- * LICENSE.txt  file.
+ * LICENSE.txt file.
  */
 
 package org.mule;
@@ -148,7 +148,7 @@ public class DefaultMuleMessage implements MuleMessage, ThreadSafeAccess, Deseri
 
     public DefaultMuleMessage(Object message, Map<String, Object> outboundProperties, Map<String, DataHandler> attachments, MuleContext muleContext)
     {
-        this(message, null, outboundProperties, attachments, muleContext);
+        this(message, null, outboundProperties, attachments, muleContext);   
     }
 
     public DefaultMuleMessage(Object message, Map<String, Object> inboundProperties,
@@ -535,6 +535,38 @@ public class DefaultMuleMessage implements MuleMessage, ThreadSafeAccess, Deseri
         assertAccess(READ);
         return getPayloadAsString(getEncoding());
     }
+
+     /**
+     * {@inheritDoc}
+     */
+    public String getPayloadForLogging(String encoding)
+    {
+    	try
+    	{
+    		return getPayloadAsString(encoding);
+    	}
+    	catch (Exception e) 
+    	{
+    		// TODO Auto-generated catch block
+    		return  "[Messaage could not be converted to string]";
+    	}
+    } 
+    
+    /**
+     * {@inheritDoc}
+     */
+    public String getPayloadForLogging()
+    {
+    	try
+    	{
+    		return getPayloadAsString();
+    	}
+    	catch (Exception e) 
+    	{
+    		// TODO Auto-generated catch block
+    		return  "[Messaage could not be converted to string]";
+    	}
+    } 
 
     /**
      * {@inheritDoc}
