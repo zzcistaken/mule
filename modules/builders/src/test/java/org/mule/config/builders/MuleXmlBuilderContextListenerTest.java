4d94e394859864bedb38cb72b1ec08fab2936033
esteban.robles <esteban.robles>
2011-01-12 21:15:53 +0000

    EE-2127: Need custom strategy to select the .mule location - Improve test
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.2.x@20986 bf997673-6b11-0410-b953-e057580c5b09

--- modules/builders/src/test/java/org/mule/config/builders/MuleXmlBuilderContextListenerTest.java
+++ modules/builders/src/test/java/org/mule/config/builders/MuleXmlBuilderContextListenerTest.java
@@ -10,13 +10,16 @@
 
 package org.mule.config.builders;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import org.mule.MuleServer;
 
 import javax.servlet.ServletContext;
 
 import junit.framework.TestCase;
 
-import org.mockito.Mockito;
 import org.springframework.web.context.WebApplicationContext;
 
 public class MuleXmlBuilderContextListenerTest extends TestCase
@@ -29,7 +32,7 @@ public class MuleXmlBuilderContextListenerTest extends TestCase
     {
         super.setUp();
         listener = new MuleXmlBuilderContextListener();
-        context = Mockito.mock(ServletContext.class);
+        context = mock(ServletContext.class);
     }
 
     public void tearDown() throws Exception
@@ -41,50 +44,50 @@ public class MuleXmlBuilderContextListenerTest extends TestCase
 
     public void testNoMuleAppProperties()
     {
-        Mockito.when(context.getInitParameter(MuleXmlBuilderContextListener.INIT_PARAMETER_MULE_CONFIG))
+        when(context.getInitParameter(MuleXmlBuilderContextListener.INIT_PARAMETER_MULE_CONFIG))
             .thenReturn("mule-config.xml");
-        Mockito.when(context.getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE))
+        when(context.getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE))
             .thenReturn(null);
 
         listener.initialize(context);
 
-        Mockito.verify(context).getInitParameter(MuleXmlBuilderContextListener.INIT_PARAMETER_MULE_CONFIG);
-        Mockito.verify(context).getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
+        verify(context).getInitParameter(MuleXmlBuilderContextListener.INIT_PARAMETER_MULE_CONFIG);
+        verify(context).getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
 
         assertEquals("./.mule", listener.muleContext.getConfiguration().getWorkingDirectory());
     }
 
     public void testWithImplicitMuleAppProperties()
     {
-        Mockito.when(context.getInitParameter(MuleXmlBuilderContextListener.INIT_PARAMETER_MULE_CONFIG))
+        when(context.getInitParameter(MuleXmlBuilderContextListener.INIT_PARAMETER_MULE_CONFIG))
             .thenReturn("org/mule/config/builders/mule-config.xml");
-        Mockito.when(context.getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE))
+        when(context.getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE))
             .thenReturn(null);
 
         listener.initialize(context);
 
-        Mockito.verify(context).getInitParameter(MuleXmlBuilderContextListener.INIT_PARAMETER_MULE_CONFIG);
-        Mockito.verify(context).getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
+        verify(context).getInitParameter(MuleXmlBuilderContextListener.INIT_PARAMETER_MULE_CONFIG);
+        verify(context).getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
 
         assertTrue(listener.muleContext.getConfiguration().getWorkingDirectory().endsWith(
-            "modules/builders/target/.appTmp"));
+            "target/.appTmp"));
     }
 
     public void testWithExplicitMuleAppProperties()
     {
-        Mockito.when(context.getInitParameter(MuleXmlBuilderContextListener.INIT_PARAMETER_MULE_CONFIG))
+        when(context.getInitParameter(MuleXmlBuilderContextListener.INIT_PARAMETER_MULE_CONFIG))
             .thenReturn("org/mule/config/builders/mule-config.xml");
-        Mockito.when(context.getInitParameter(MuleXmlBuilderContextListener.INIT_PARAMETER_MULE_APP_CONFIG))
+        when(context.getInitParameter(MuleXmlBuilderContextListener.INIT_PARAMETER_MULE_APP_CONFIG))
         .thenReturn("org/mule/config/builders/mule-app-ppp.properties");
-        Mockito.when(context.getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE))
+        when(context.getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE))
             .thenReturn(null);
 
         listener.initialize(context);
 
-        Mockito.verify(context).getInitParameter(MuleXmlBuilderContextListener.INIT_PARAMETER_MULE_CONFIG);
-        Mockito.verify(context).getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
+        verify(context).getInitParameter(MuleXmlBuilderContextListener.INIT_PARAMETER_MULE_CONFIG);
+        verify(context).getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
 
         assertTrue(listener.muleContext.getConfiguration().getWorkingDirectory().endsWith(
-            "modules/builders/target/.appTmp2"));
+            "target/.appTmp2"));
     }
 }
