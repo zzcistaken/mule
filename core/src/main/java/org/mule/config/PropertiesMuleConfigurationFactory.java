e49bed4fe49d717610c7b0ee540608e8ab37f3ce
esteban.robles <esteban.robles>
2011-01-12 11:59:28 +0000

    EE-2127: Need custom strategy to select the .mule location
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.2.x@20966 bf997673-6b11-0410-b953-e057580c5b09

--- core/src/main/java/org/mule/config/PropertiesMuleConfigurationFactory.java
+++ core/src/main/java/org/mule/config/PropertiesMuleConfigurationFactory.java
@@ -11,19 +11,19 @@
 package org.mule.config;
 
 import org.mule.MuleServer;
-import org.mule.api.config.MuleConfiguration;
+import org.mule.util.BeanUtils;
 import org.mule.util.ClassUtils;
 import org.mule.util.FilenameUtils;
-import org.mule.util.NumberUtils;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
 import java.util.Properties;
+import java.util.Map.Entry;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang.BooleanUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -80,77 +80,32 @@ public class PropertiesMuleConfigurationFactory
 
     private void initializeFromProperties(DefaultMuleConfiguration configuration)
     {
-        String p;
+        for (Entry<Object, Object> entry : this.properties.entrySet())
+        {
+            String key = (String) entry.getKey();
+            String value = (String) entry.getValue();
 
-        p = this.properties.getProperty(MuleConfiguration.SYSTEM_PROPERTY_PREFIX + "encoding");
-        if (p != null)
-        {
-            configuration.setDefaultEncoding(p);
-        }
-        p = this.properties.getProperty(MuleConfiguration.SYSTEM_PROPERTY_PREFIX + "endpoints.synchronous");
-        if (p != null)
-        {
-            configuration.setDefaultSynchronousEndpoints(BooleanUtils.toBoolean(p));
-        }
-        p = this.properties.getProperty(MuleConfiguration.SYSTEM_PROPERTY_PREFIX + "systemModelType");
-        if (p != null)
-        {
-            configuration.setSystemModelType(p);
-        }
-        p = this.properties.getProperty(MuleConfiguration.SYSTEM_PROPERTY_PREFIX + "timeout.synchronous");
-        if (p != null)
-        {
-            configuration.setDefaultResponseTimeout(NumberUtils.toInt(p));
-        }
-        p = this.properties.getProperty(MuleConfiguration.SYSTEM_PROPERTY_PREFIX + "timeout.transaction");
-        if (p != null)
-        {
-            configuration.setDefaultTransactionTimeout(NumberUtils.toInt(p));
-        }
-        p = this.properties.getProperty(MuleConfiguration.SYSTEM_PROPERTY_PREFIX + "workingDirectory");
-        if (p != null)
-        {
-            configuration.setWorkingDirectory(p);
-        }
-        p = this.properties.getProperty(MuleConfiguration.SYSTEM_PROPERTY_PREFIX + "clientMode");
-        if (p != null)
-        {
-            configuration.setClientMode(BooleanUtils.toBoolean(p));
-        }
-        p = this.properties.getProperty(MuleConfiguration.SYSTEM_PROPERTY_PREFIX + "serverId");
-        if (p != null)
-        {
-            configuration.setId(p);
-        }
-        p = this.properties.getProperty(MuleConfiguration.SYSTEM_PROPERTY_PREFIX + "clusterId");
-        if (p != null)
-        {
-            configuration.setClusterId(p);
-        }
-        p = this.properties.getProperty(MuleConfiguration.SYSTEM_PROPERTY_PREFIX + "domainId");
-        if (p != null)
-        {
-            configuration.setDomainId(p);
-        }
-        p = this.properties.getProperty(MuleConfiguration.SYSTEM_PROPERTY_PREFIX + "message.cacheBytes");
-        if (p != null)
-        {
-            configuration.setCacheMessageAsBytes(BooleanUtils.toBoolean(p));
-        }
-        p = this.properties.getProperty(MuleConfiguration.SYSTEM_PROPERTY_PREFIX + "message.cacheOriginal");
-        if (p != null)
-        {
-            configuration.setCacheMessageOriginalPayload(BooleanUtils.toBoolean(p));
-        }
-        p = this.properties.getProperty(MuleConfiguration.SYSTEM_PROPERTY_PREFIX + "streaming.enable");
-        if (p != null)
-        {
-            configuration.setEnableStreaming(BooleanUtils.toBoolean(p));
-        }
-        p = this.properties.getProperty(MuleConfiguration.SYSTEM_PROPERTY_PREFIX + "transform.autoWrap");
-        if (p != null)
-        {
-            configuration.setAutoWrapMessageAwareTransform(BooleanUtils.toBoolean(p));
+            if (key.startsWith("sys."))
+            {
+                String systemProperty = key.substring(4);
+                System.setProperty(systemProperty, value);
+            }
+            else if (key.startsWith("mule.config."))
+            {
+                String configProperty = key.substring(12);
+                try
+                {
+                    BeanUtils.setProperty(configuration, configProperty, value);
+                }
+                catch (IllegalAccessException e)
+                {
+                    logger.error(e);
+                }
+                catch (InvocationTargetException e)
+                {
+                    logger.error(e);
+                }
+            }
         }
     }
 }
