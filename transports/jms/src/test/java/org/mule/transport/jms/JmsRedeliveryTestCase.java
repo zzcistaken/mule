0b601a001680d2b7345b863025b5eb1f072d1932
Pablo Kraan <pablo.kraan@gmail.com>
2011-12-01 15:43:10 +0000

    MULE-5943: maxRedelivery -1 not being taken into account by ActiveMQ Connector
    ActiveMQ uses the redelivery count in a wrong way, ActiveMQJmsConnector adjusts that
    value, but in an incorrect way that causes that "infinite redelivery" (-1) being
    changed to "no redelivery" (0)
    
    
    git-svn-id: https://svn.codehaus.org/mule/branches/mule-2.2.x@23434 bf997673-6b11-0410-b953-e057580c5b09

--- transports/jms/src/test/java/org/mule/transport/jms/JmsRedeliveryTestCase.java
+++ transports/jms/src/test/java/org/mule/transport/jms/JmsRedeliveryTestCase.java
@@ -11,9 +11,11 @@
 package org.mule.transport.jms;
 
 import org.mule.api.MuleEventContext;
+import org.mule.api.MuleException;
 import org.mule.api.context.notification.ExceptionNotificationListener;
 import org.mule.api.context.notification.ServerNotification;
 import org.mule.context.notification.ExceptionNotification;
+import org.mule.context.notification.NotificationException;
 import org.mule.module.client.MuleClient;
 import org.mule.tck.FunctionalTestCase;
 import org.mule.tck.exceptions.FunctionalTestException;
@@ -25,28 +27,73 @@ import edu.emory.mathcs.backport.java.util.concurrent.TimeUnit;
 
 public class JmsRedeliveryTestCase extends FunctionalTestCase
 {
-    private final int timeout = getTimeoutSecs() * 1000 / 4;
-    private static final String DESTINATION = "jms://in";
 
+    private final int timeout = getTimeoutSecs() * 1000 / 4;
+
+    private Latch redeliveryExceptionFire = new Latch();
+    private MuleClient client;
+    private CounterCallback callback;
+
+    @Override
     protected String getConfigResources()
     {
         return "jms-redelivery.xml";
     }
 
+    @Override
+    protected void doSetUp() throws Exception
+    {
+        client = new MuleClient();
+        callback = createEventCallback();
+        registerEventListener(redeliveryExceptionFire);
+    }
+
     public void testRedelivery() throws Exception
     {
-        MuleClient client = new MuleClient();
-        // required if broker is not restarted with the test - it tries to deliver those messages to the client
-        // purge the queue
-        while (client.request(DESTINATION, 1000) != null)
-        {
-            logger.warn("Destination " + DESTINATION + " isn't empty, draining it");
-        }
+        String destination = "jms://in1?connector=jmsConnectorLimitedRedelivery";
+        purgeQueue(destination);
+        setupCallback("Bouncer1");
 
-        FunctionalTestComponent ftc = getFunctionalTestComponent("Bouncer");
+        client.dispatch(destination, "test", null);
 
-        // whether a MessageRedeliverdException has been fired
-        final Latch mrexFired = new Latch();
+        assertTrue(redeliveryExceptionFire.await(timeout, TimeUnit.MILLISECONDS));
+        assertEquals("MessageRedeliveredException never fired.", 0, redeliveryExceptionFire.getCount());
+        assertEquals("Wrong number of delivery attempts", 4, callback.getCallbackCount());
+    }
+
+    public void testInfiniteRedelivery() throws Exception
+    {
+        String destination = "jms://in2?connector=jmsConnectorInfiniteRedelivery";
+        purgeQueue(destination);
+        setupCallback("Bouncer2");
+
+        client.dispatch(destination, "test", null);
+
+        assertFalse(redeliveryExceptionFire.await(timeout, TimeUnit.MILLISECONDS));
+        assertEquals("MessageRedeliveredException was fired.", 1, redeliveryExceptionFire.getCount());
+        assertTrue(callback.getCallbackCount() > 6);
+    }
+
+    public void testNoRedelivery() throws Exception
+    {
+        purgeQueue("jms://in3?connector=jmsConnectorNoRedelivery");
+        setupCallback("Bouncer3");
+
+        client.dispatch("jms://in3?connector=jmsConnectorNoRedelivery", "test", null);
+
+        assertTrue(redeliveryExceptionFire.await(timeout, TimeUnit.MILLISECONDS));
+        assertEquals("MessageRedeliveredException never fired.", 0, redeliveryExceptionFire.getCount());
+        assertEquals(1, callback.getCallbackCount());
+    }
+
+    private void setupCallback(String serviceName) throws Exception
+    {
+        FunctionalTestComponent component = (FunctionalTestComponent) getComponent(serviceName);
+        component.setEventCallback(callback);
+    }
+
+    private void registerEventListener(final Latch redeliveryExceptionFire) throws NotificationException
+    {
         muleContext.registerListener(new ExceptionNotificationListener()
         {
             public void onNotification(ServerNotification notification)
@@ -54,29 +101,34 @@ public class JmsRedeliveryTestCase extends FunctionalTestCase
                 ExceptionNotification n = (ExceptionNotification) notification;
                 if (n.getException() instanceof MessageRedeliveredException)
                 {
-                    mrexFired.countDown();
+                    redeliveryExceptionFire.countDown();
                 }
             }
         });
+    }
 
+    private CounterCallback createEventCallback()
+    {
         // enhance the counter callback to count, then throw an exception
-        final CounterCallback callback = new CounterCallback()
+        return new CounterCallback()
         {
             @Override
             public void eventReceived(MuleEventContext context, Object Component) throws Exception
             {
                 final int count = incCallbackCount();
-                logger.info("Message Delivery Count is: " + count); 
+                logger.info("Message Delivery Count is: " + count);
                 throw new FunctionalTestException();
             }
         };
-        ftc.setEventCallback(callback);
-
-        client.dispatch(DESTINATION, "test", null);
-
-        mrexFired.await(timeout, TimeUnit.MILLISECONDS);
-        assertEquals("MessageRedeliveredException never fired.", 0, mrexFired.getCount());
-        assertEquals("Wrong number of delivery attempts", 4, callback.getCallbackCount());
     }
 
+    private void purgeQueue(String destination) throws MuleException
+    {
+        // required if broker is not restarted with the test - it tries to deliver those messages to the client
+        // purge the queue
+        while (client.request(destination, 1000) != null)
+        {
+            logger.warn("Destination " + destination + " isn't empty, draining it");
+        }
+    }
 }
